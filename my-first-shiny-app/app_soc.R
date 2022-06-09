312022c959b3c70bc98555040ad000f01678f1c9
Neli <neliquinhonez92@gmail.com>
2022-06-09 05:09:47 -0300

    Update app_soc.R

--- my-first-shiny-app/app_soc.R
+++ my-first-shiny-app/app_soc.R
@@ -3,17 +3,38 @@ library(shiny)
 ## User interface
 ui <- fluidPage(
   titlePanel('Pilot platform for SOC monitoring'),
+  
   sidebarLayout(
-    sidebarPanel(),
+    sidebarPanel(
+      helpText('Display SOC maps. Baseline and predictions available.'),
+      selectInput('var',
+                  label = 'Choose the map to display',
+                  choices = list("Baseline SOC",
+                                 'SOC prediction of two years',
+                                 'SOC prediction of four years',
+                                 'SOC prediction of six years',
+                                 'SOC prediction of eight years',
+                                 'Uncertainty',
+                                 'Additionality'),
+                  selected = 'None')
+    ),
     mainPanel(
-      img(src = 'C:/Users/neliq/Desktop/example_map.PNG', height = 500, width = 350)
+      tabsetPanel(
+        tabPanel('Maps', imageOutput("myImage")),
+        tabPanel('Summary', verbatimTextOutput("summary")),
+        tabPanel('Table', tableOutput('table'))
     )
   )
-)  
+)
+)
 
 
 ## Server
-server <- function(input, output) { }
+server <- function(input, output) {
+  output$myImage <- renderImage({
+    list(src = 'www/example_map.png',
+         height = 800, width = 600)
+  }, deleteFile = F)
+  }
 
 shinyApp(ui = ui, server = server)
-
